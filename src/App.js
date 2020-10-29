f1a4f5117438ef4e717f0f29b2d69f95f714f383
AliyaBest <aliyabest@gmail.com>
2020-10-29 17:17:34 -0400

    Feature: next button, disabled buttons on click and added answer feedback messages and functionality

--- src/App.js
+++ src/App.js
@@ -5,22 +5,38 @@ const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false)
   const [score, setScore] = useState(0)
+  const [isCorrect, setIsCorrect] = useState()
+  const [buttonIsDisabled, setButtonIsDisabled] = useState(false)
   const correctAnswerPlacement = Math.floor((Math.random() * 4) + 1);
+  const [answerFeedback, setAnswerFeedback] = useState('')
+  let correctAnswer = ''
 
   const handleAnswerButton = (isCorrect)=>{
     if(isCorrect){
       setScore(score + 1)
-      alert('correct')
+      setIsCorrect(true)
+      setAnswerFeedback('Correct')
     } else {
-      alert('incorrect')
+      setIsCorrect(false)
+      setAnswerFeedback(`Sorry! That's incorrect. The correct answer is ${correctAnswer}`)
+
     }
+
+    setButtonIsDisabled(true)
+   }
+
+
+  const handleNextButton = ()=>{
     const nextQuestion = currentQuestion + 1
 
     if(nextQuestion < data.length){
       setCurrentQuestion(nextQuestion)
     } else {
       setShowScore(true)
+      document.getElementById('next-button').style.display = "none"
     }
+    setButtonIsDisabled(false)
+    setAnswerFeedback('')
 
   }
 
@@ -36,19 +52,28 @@ const App = () => {
 
           if(id === currentQuestion){
             const answers = element.incorrect.slice()
+
+            correctAnswer = element.correct
+
+
             answers.splice(correctAnswerPlacement, 0, element.correct)
             return(
-            <div>
+            <div className="answers">
             <h3 key={id}>{element.question}</h3>
             {answers.map(answer=>{
 
-             return <button onClick={()=>handleAnswerButton(!element.incorrect.includes(answer))}>{answer}</button>
+             return <div><ul><button disabled={buttonIsDisabled} className="answer_buttons" onClick={()=>handleAnswerButton(!element.incorrect.includes(answer))}><li>{answer}</li></button></ul>
+             </div>
             })}
+            <p>{answerFeedback}</p>
             </div>
             )
           }
 
         })}
+      </section>
+      <section>
+        <button id="next-button" onClick={()=>handleNextButton()}>Next</button>
       </section>
 
     </div>
